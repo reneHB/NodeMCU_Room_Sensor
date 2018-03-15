9d34fdf7e7dc8f8324de2a8242110acf04ea3058
René Reimann <rreimann93@icloud.com>
2018-03-15 19:40:53 +0100

    hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+void setup()
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    delay(1000);
+    Serial.println("Hello World!");
 }
