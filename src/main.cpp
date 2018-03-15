a313fcbc7dd9b0283af0cd2423d16288d89cc653
René Reimann <rreimann93@icloud.com>
2018-03-15 21:33:15 +0100

    hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
+/**
+ * @file main.cpp
+ * @author René Reimann
+ * @date 15.03.2018
+ * @brief File containing code.
+ *
+ * extensive explanation
+ *
+ * @copyright (C) 2018 René Reimann <rreimann93@icloud.com>
+ *
+ * @license This project is released under the MIT License.
+ *
+ * @see https://github.com/reneHB/NodeMCU_Room_Sensor
+ */
+
 #include <Arduino.h>
+
+
+
 
 void setup()
 {
